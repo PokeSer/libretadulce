80c11156e30a60d18bedcaf6e59dd1e5d789d499
PokeSer <serga@github.com>
2026-05-28 16:30:49 +0200

    Explicitly set Google Sign-In web client ID for release builds

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,7 +4,9 @@ import 'package:flutter/foundation.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: '719361605717-i40g4vdoa3e1cdl5i9rccq615bmt6o4s.apps.googleusercontent.com',
+  );
 
   // Iniciar sesión con Google
   Future<UserCredential?> signInWithGoogle() async {
